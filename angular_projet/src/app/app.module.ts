a17b867adfae4bd1fd64c025826db66e849bdb6b
Eric LEFEBVRE <e2405459@ensimlan.univ-lemans.fr>
2025-04-30 10:32:07 +0200

    eliott est éclaté

--- angular_projet/src/app/app.module.ts
+++ angular_projet/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
