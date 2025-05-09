89a4ee54846c1a9eed8252c26adef250b13a30f0
Eric LEFEBVRE <e2405459@ensimlan.univ-lemans.fr>
2025-05-09 14:59:56 +0200

    composant conception simple

---
+++ angular_projet/src/app/conception-simple/conception-simple.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conception-simple',
+  standalone: false,
+  templateUrl: './conception-simple.component.html',
+  styleUrl: './conception-simple.component.css'
+})
+export class ConceptionSimpleComponent {
+
+}
